8e15dd6cb90852e9f69478b6f84fea41ba152f9e
asamaiya00 <asamaiya00@gmail.com>
2020-07-25 01:10:10 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
-    <div className="App">
-      My App
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
